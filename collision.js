e4c37319a8cdba781fe04dbbfb29b7125454d3aa
JabirASC6 <jemeka11@icloud.com>
2019-07-18 11:25:48 -0400

    first commit

--- collision.js
+++ collision.js
@@ -38,7 +38,7 @@ function setup(){
 
 function draw(){
     background(0)
-    image(backdrop, 0 ,0, 500 )
+    image(backdrop, 500, 500 )
     // fill(255,0,0)
     // rect(enemyX,enemyY,enemyW,enemyH)
     // fill(0,0,255)
